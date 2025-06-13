b7d5b59151b9d11b23ce44bb3719843c45aed82b
Valdecir Hideki Nakao <valdecir.nakao@fatec.sp.gov.br>
2025-06-13 18:58:07 -0300

    feat(p2): converte hora para horario brasileiro

--- previsao-do-tempo/src/components/Previsao.jsx
+++ previsao-do-tempo/src/components/Previsao.jsx
@@ -1,9 +1,14 @@
 import striptags from 'striptags'
 
-const Previsao = ({dataHora, temperaturaMinima, temperaturaMaxima, umidadeRelativa, icone, descricao}) => {   
+const Previsao = ({dataHora, temperaturaMinima, temperaturaMaxima, umidadeRelativa, icone, descricao}) => {    
+  const dateUtc = new Date(dataHora.replace(" ", "T") + "Z");
+  const dataHoraBrasil = dateUtc.toLocaleString("pt-BR", {
+  timeZone: "America/Sao_Paulo",
+  hour12: false,
+});
   return (
     <div className='text-center col-12 p-3 border-1 m-2'>
-      <h2>{striptags(dataHora)}</h2>
+      <h2>{striptags(dataHoraBrasil)}</h2>
       <p>Temperatura Mínima: {temperaturaMinima}°C</p>
       <p>Temperatura Máxima: {temperaturaMaxima}°C</p>
       <p>Umidade Relativa do Ar: {umidadeRelativa}%</p>
